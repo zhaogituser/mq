d37d7c23dbf6e396e518ce2e8066c1a15fb7105d
oyd714 <1642185045@qq.com>
2017-11-10 11:27:40 +0800

    1.位于分支testB

---
+++ src/test/com/test/TestB.java
@@ -0,0 +1,10 @@
+package com.test;
+
+/**
+ * Created by ${zhao} on 2017/11/10 0010.
+ */
+public class TestB {
+    public static void main(String[] args) {
+        System.out.println("这是分支Testb");
+    }
+}
